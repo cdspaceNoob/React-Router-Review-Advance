a4d42660f9aa452f20c1d166809c4f433f1c6ec7
voyager <cdspacenoob@gmail.com>
2023-03-09 19:08:31 +0900

    use loader's parameter present by ReactRouter

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,15 +1,25 @@
-import { Fragment } from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
 
 const EventDetailPage = () => {
-  const params = useParams();
+  const loadedData = useLoaderData();
 
-  return (
-    <Fragment>
-      <h1> Event Detail Page. </h1>
-      <h4> The ID of this Event is ... {params.eventID}</h4>
-    </Fragment>
-  );
+  return <EventItem event={loadedData.event} />;
 };
 
 export default EventDetailPage;
+
+export const loader = async ({ request, params }) => {
+  const id = params.eventID;
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "cannot fetch detail data" },
+      { status: 500, statusText: "Request URL invalid" }
+    );
+  } else {
+    return response;
+  }
+};
